be15cedc21f01612942b838cf04ee4607e458ab0
Omar Gonzalez <gonzalezrocha.omar@gmail.com>
2017-10-01 12:06:12 -0500

    project set up

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn straymap.wsgi
---
+++ straymap/static/src/app.js
@@ -0,0 +1,15 @@
+/**
+* Utils
+*/
+const l = function(log){
+    console.log(log);
+}
+
+/**
+* Stray Map Custom JS:
+*/
+
+$(document).on('pageinit', function(){
+    l('stray map - dev');
+});
+
