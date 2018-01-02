588320dddc146af63f795326d5b61cd2dc2b8833
Omar Gonzalez <gonzalezrocha.omar@gmail.com>
2018-01-01 21:47:50 -0600

    SSL REDIRECT TRUE

--- straymap/settings.py
+++ straymap/settings.py
@@ -101,7 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-SECURE_SSL_REDIRECT = False
+SECURE_SSL_REDIRECT = True
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
